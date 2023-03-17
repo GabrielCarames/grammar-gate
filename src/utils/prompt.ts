5974063dd3fcfa418561791ded8f60790ce7f4c7
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-17 12:47:25 -0300

    refactor(prompt): use new prompt

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -3,11 +3,21 @@ const createPrompt = (textToCheck: string) => {
 Necesito que hagas de corrector gramatical de ingles. Te daré un texto en ingles y tendras que corregirlo indicando que palabra/s estan mal, como deberian ser corregidas, y él por que.
 Mas allá del significado correcto del texto, tambien necesitaría que indiques donde podrían ir comas, puntos, etc.
 
-Debes responder de esta manera:
-Texto original: "texto ingresado"
-Texto a corregir: "lista de palabras u oraciones a corregir"
-Explicación del texto a corregir: "explicación de cada una de las palabras u oraciones de la lista anterior"
-Texto corregido: "texto corregido"
+Necesito que me devuelvas un Array de JavaScript, en donde cada punto sea un Object. Como este ejemplo:
+[
+   {
+      "id": "id unica generada por ti",
+      "type": "[tipo de correción] mistake",
+      "explanation": "explanation",
+      "result": ["palabra u oración que tengas que corregir", "palabra u oración ya corregida"]
+   },
+ {
+      "id": "id unica generada por ti",
+      "type": "[tipo de correción] mistake",
+      "explanation": "explanation",
+      "result": ["palabra u oración que tengas que corregir", "palabra u oración ya corregida"]
+   }
+]
 
 Este es el texto para corregir: ${textToCheck}
 `
