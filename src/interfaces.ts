d8a5d504866b8791d71b16f1a69298997d93cf80
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-17 14:30:34 -0300

    feat: add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,27 @@
+import { AxiosHeaders } from "axios"
+
+export interface MessageProps {
+  role: string
+  content: string
+}
+
+export interface CorrectionProps {
+  id: string
+  type: string
+  explanation: string
+  result: string[]
+}
+
+export interface MakeRequestProps {
+  (
+    url: string,
+    payload: ChatGPTJSON_dataProps,
+    headers: AxiosHeaders["Content-Type, Authorization"]
+  ): Promise<any>
+}
+
+export interface ChatGPTJSON_dataProps {
+  model: string
+  messages: MessageProps[]
+  temperature: number
+}
