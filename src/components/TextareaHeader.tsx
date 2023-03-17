a9ed7214ac965e28b8be98375ae578bd09f25520
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-17 18:24:30 -0300

    feat: add TextareaHeader component

---
+++ src/components/TextareaHeader.tsx
@@ -0,0 +1,10 @@
+export default function TextareaHeader() {
+  return (
+    <header className="bg-pink-1 py-8 px-5 h-max mb-10 rounded-md">
+      <h1 className="text-xl font-bold leading-none">
+        Grammar Checker |{" "}
+        <span className="font-normal">Write your text and let us check your grammar!</span>
+      </h1>
+    </header>
+  )
+}
