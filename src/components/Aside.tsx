32c4934559facceb0894b8cba36c8dbe73799ab0
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-17 17:43:36 -0300

    fix(Aside): fix setCorrections functionallity

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,4 +1,4 @@
-import { CorrectionProps } from "@/interfaces"
+import { CorrectionProps, CorrectionsProps } from "@/interfaces"
 import { Dispatch, SetStateAction } from "react"
 
 export default function Aside({
@@ -6,28 +6,33 @@ export default function Aside({
   setValue,
   setCorrections
 }: {
-  corrections: CorrectionProps[]
+  corrections: CorrectionsProps
   setValue: Dispatch<SetStateAction<string>>
-  setCorrections: Dispatch<SetStateAction<CorrectionProps[]>>
+  setCorrections: Dispatch<SetStateAction<CorrectionsProps>>
 }) {
   const handleCorrection = (correction: CorrectionProps) => {
     setValue((prev: string) => {
       const [before, after] = prev.split(correction.result[0])
       return `${before}${correction.result[1]}${after}`
     })
-    setCorrections((prev: CorrectionProps[]) =>
-      prev.filter((prevCorrection: CorrectionProps) => prevCorrection.id !== correction.id)
-    )
+    setCorrections((prev: CorrectionsProps) => {
+      return {
+        corrections: prev.corrections.filter(
+          (prevCorrection: CorrectionProps) => prevCorrection.id !== correction.id
+        ),
+        textCorrected: prev.textCorrected
+      }
+    })
   }
 
   return (
     <aside className="w-80 bg-gray-1 p-5">
       <h2 className="text-lg mb-10">General corrections</h2>
-      {corrections?.length === 0 && (
+      {corrections.corrections?.length === 0 && (
         <p className="text-center">No corrections available. Well done!</p>
       )}
       <ul className="flex flex-col gap-10">
-        {corrections?.map((correction: CorrectionProps) => (
+        {corrections?.corrections?.map((correction: CorrectionProps) => (
           <li className="flex flex-col gap-2" key={correction.id}>
             <p>{correction.type}</p>
             <p>{correction.explanation}</p>
