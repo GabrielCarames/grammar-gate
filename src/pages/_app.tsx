4e96813a1ab24510346d28def3e9ab8e327056e9
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-17 18:13:41 -0300

    feat: add CorrectionsContext context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { CorrectionsProvider } from "@/contexts/CorrectionsContext"
 import { ValueProvider } from "@/contexts/ValueContext"
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
@@ -5,7 +6,9 @@ import type { AppProps } from "next/app"
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ValueProvider>
-      <Component {...pageProps} />
+      <CorrectionsProvider>
+        <Component {...pageProps} />
+      </CorrectionsProvider>
     </ValueProvider>
   )
 }
