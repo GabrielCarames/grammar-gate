699324e9a3aa63260a94ebacb847215fc2b1ff04
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-17 12:48:34 -0300

    feat(aside): add handleCorrection function

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,10 +1,29 @@
-export default function Aside({ corrections }: { corrections: any }) {
+export default function Aside({
+  corrections,
+  setValue,
+  setCorrections
+}: {
+  corrections: any
+  setValue: any
+  setCorrections: any
+}) {
   console.log(corrections)
+
+  const handleCorrection = (correction: any) => {
+    setValue((prev: string) => {
+      const [before, after] = prev.split(correction.result[0])
+      return `${before}${correction.result[1]}${after}`
+    })
+    setCorrections((prev: any) => prev.filter((c: any) => c.id !== correction.id))
+  }
 
   return (
     <aside className="w-80 bg-gray-1 p-5">
-      <h2 className="text-lg">General corrections</h2>
-      <ul className="mt-10 flex flex-col gap-10">
+      <h2 className="text-lg mb-10">General corrections</h2>
+      {corrections?.length === 0 && (
+        <p className="text-center">No corrections available. Well done!</p>
+      )}
+      <ul className="flex flex-col gap-10">
         {corrections?.map((correction: any) => (
           <li className="flex flex-col gap-2" key={correction.id}>
             <p>{correction.type}</p>
@@ -13,7 +32,9 @@ export default function Aside({ corrections }: { corrections: any }) {
               <p className="line-through text-red-500">{correction.result[0]}</p>
               <p className="text-green-500">{correction.result[1]}</p>
             </div>
-            <button className="bg-orange-2">Correct</button>
+            <button className="bg-orange-2" onClick={() => handleCorrection(correction)}>
+              Correct
+            </button>
           </li>
         ))}
       </ul>
