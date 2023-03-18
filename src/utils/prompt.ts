b6ade55e452773f5a88cd6e63a1683b932c0148b
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-18 12:31:03 -0300

    refactor(prompt): add createConsecutivePrompt function

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,10 +1,9 @@
-const createPrompt = (textToCheck: string) => {
-  const prompt = `
+export const createFirstPrompt = (textToCheck: string) => {
+  const firstPrompt = `
     Necesito que hagas de corrector gramatical de ingles. Te daré un texto en ingles y tendras que corregirlo indicando que palabra/s estan mal, como deberian ser corregidas, y él por que.
     Mas allá del significado correcto del texto, tambien necesitaría que indiques donde podrían ir comas, puntos, etc.
-
-    Necesito que SIEMPRE y SOLO me respondas con un JSON como vas a ver en el ejemplo de abajo. Nunca me respondas con otra cosa ni me expliques nada sin que yo te lo pida
-    Tambien recuerda agregar las comas luego de cada Object, y no agregar una coma al final del Array. 
+    Necesito que SIEMPRE y SOLO me respondas con un JSON como vas a ver en el ejemplo de abajo. Nunca me respondas con otra cosa ni me expliques nada sin que yo te lo pida.
+    Si hay has corregido anteriormente, y el nuevo texto incluye lo mismo que el campo "textCorrected" pero con nuevas palabras u oraciones, no corrigas lo del campo "textCorrected" y solo corriges lo nuevo.
 
     Fijate en este ejemplo de como deberias devolverme la respuesta:
 
@@ -28,7 +27,15 @@ const createPrompt = (textToCheck: string) => {
 
     Este es el texto para corregir: ${textToCheck}
   `
-  return prompt
+  return firstPrompt
 }
 
-export default createPrompt
+export const createConsecutivePrompt = (textToCheck: string, previousTextCorrected: string) => {
+  const consecutivePrompt = `
+    Ahora corrige este nuevo texto pero ten en cuenta que te voy a dar el texto anterior que corregiste, por lo que si este nuevo texto ya contiene el texto corregido, no lo corrigas de nuevo.
+    
+    Texto que corregiste en la anterior petición: ${previousTextCorrected}
+    Nuevo texto para corregir: ${textToCheck}
+  `
+  return consecutivePrompt
+}
