fa111b215e2be5c9de0fd9d7ea7c4d4e9459f2eb
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-18 14:59:14 -0300

    feat(app): add ToastContainer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { CorrectionsProvider } from "@/contexts/CorrectionsContext"
 import { ValueProvider } from "@/contexts/ValueContext"
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ValueProvider>
       <CorrectionsProvider>
+        <ToastContainer />
         <Component {...pageProps} />
       </CorrectionsProvider>
     </ValueProvider>
