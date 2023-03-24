52a10d701c0ca6f4bc23cb9d97e3a203af21f66d
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-24 17:39:20 -0300

    feat: add Modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,20 @@
+import { XCircleIcon } from "@heroicons/react/24/solid"
+import ReactDOM from "react-dom"
+
+function Modal({ onClose, children }: { onClose: () => void; children: React.ReactNode }) {
+  return ReactDOM.createPortal(
+    <div className="fixed inset-0 flex justify-center items-center w-full h-screen z-10 bg-black bg-opacity-70">
+      <section className="absolute w-full max-w-[400px] h-[650px] bg-gray-2 z-10 px-1">
+        <div className="h-full relative p-5">
+          <button className="absolute right-5 top-5" onClick={onClose}>
+            <XCircleIcon className="w-10 text-pink-300 hover:text-pink-400 duration-150" />
+          </button>
+          {children}
+        </div>
+      </section>
+    </div>,
+    document.getElementById("modal-root")!
+  )
+}
+
+export default Modal
