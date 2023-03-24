63f1369dd7524b12548b1494f3f6d9a2fcc4aa93
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-24 17:36:52 -0300

    feat(index): add modal-root container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex flex-wrap gap-y-10 md:flex-nowrap bg-black text-white w-full h-screen pt-10 md:pt-0">
-        <div className="w-full h-max md:h-full flex justify-center items-center px-5">
+        <div id="modal-root"></div>
+        <div className="w-full h-max md:h-full flex justify-center items-center px-5 relative">
           <TextareaSection />
         </div>
         <Aside />
