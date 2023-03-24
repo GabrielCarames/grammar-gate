2e6e2a3d4f897196a129d413fb090aa302b43318
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-24 07:50:24 -0300

    refactor(Correction): use CorrectionComparation component

--- src/components/Correction.tsx
+++ src/components/Correction.tsx
@@ -1,5 +1,5 @@
 import { CorrectionProps } from "@/interfaces"
-import { ArrowRightIcon } from "@heroicons/react/24/solid"
+import CorrectionComparation from "./CorrectionComparation"
 
 export default function Correction({
   correction,
@@ -11,11 +11,7 @@ export default function Correction({
   return (
     <li className="flex flex-col gap-2 border-[2px] border-gray-2 p-2 rounded-md">
       <p className="capitalize font-bold">{correction.type}</p>
-      <div className="flex gap-5">
-        <p className="line-through text-red-500">{correction?.result[0]}</p>
-        <ArrowRightIcon className="h-6 w-6 text-pink-1" />
-        <p className="text-green-500">{correction?.result[1]}</p>
-      </div>
+      <CorrectionComparation correction={correction} />
       <p>{correction.explanation}</p>
       <button
         className="bg-pink-1 text-center py-1 rounded-md hover:bg-pink-2 duration-200"
