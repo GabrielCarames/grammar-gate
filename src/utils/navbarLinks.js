44084f771cd00416f76ca37e10fa0fb679ac10bc
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-25 14:32:21 -0300

    feat: export navbarLinks array

---
+++ src/utils/navbarLinks.js
@@ -0,0 +1,19 @@
+import grammarCheckerIcon from "../../public/grammar-checker-icon.svg"
+import textSummarizer from "../../public/text-summarizer-icon.svg"
+
+const links = [
+  {
+    name: "Grammar Checker",
+    href: "/grammar-checker",
+    icon: grammarCheckerIcon,
+    backgroundColor: "bg-pink-1"
+  },
+  {
+    name: "Text Summarizer",
+    href: "/text-summarizer",
+    icon: textSummarizer,
+    backgroundColor: "bg-green-500"
+  }
+]
+
+export default links
