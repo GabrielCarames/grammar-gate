547c082e1ee1aa8d6cba4ee57e480f6ff4a71b65
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-25 19:09:30 -0300

    feat(home): add responsive styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head"
-
 import homeText from "../utils/homeText.json"
 import links from "../utils/navbarLinks"
 import NavbarLink from "@/components/NavbarLink"
@@ -13,22 +12,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex justify-center items-center bg-black text-white w-full h-screen">
-        <section className="flex flex-col gap-20 w-max h-96">
-          <header className="flex flex-col gap-5 items-center justify-center w-[900px]">
-            <h1 className="text-3xl text-pink-1">{homeText?.title}</h1>
-            <p className="text-center text-lg font-semibold">{homeText?.subititle}</p>
+      <main className="flex justify-center items-center bg-black text-white w-full min-h-screen h-max">
+        <section className="flex flex-col gap-20 w-full max-w-max h-full px-5 py-10">
+          <header className="flex flex-col gap-5 items-center justify-center w-full max-w-[900px]">
+            <h1 className="text-2xl sm:text-3xl text-pink-1 text-center">{homeText?.title}</h1>
+            <p className="text-center text-base sm:text-lg font-semibold">{homeText?.subititle}</p>
           </header>
-          <ul className="h-max flex justify-center gap-20 font-bold">
+          <ul className="h-max flex flex-wrap justify-center gap-x-20 gap-y-5 font-bold">
             {links.map((link, index) => (
-              <div className="w-max h-max flex flex-col gap-1" key={index}>
+              <div
+                className="w-max h-max flex flex-col gap-1 border-[2px] border-gray-1 p-5 rounded-md"
+                key={index}
+              >
                 <NavbarLink
                   link={link}
-                  imageDivClassName="!p-5"
-                  imageClassName="!w-10 !h-10"
+                  imageDivClassName="p-4 sm:!p-5"
+                  imageClassName="w-5 h-5 sm:!w-10 sm:!h-10"
                   linkNameClassName="!text-lg"
                 />
-                <p className="w-full max-w-[400px] font-normal text-center">
+                <p className="w-full max-w-[400px] font-normal text-center text-sm">
                   {homeText.functionalities[index].description}
                 </p>
               </div>
