db542729f9e829fc0ec9a7a9ea3b2d7985b17721
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-25 18:57:15 -0300

    feat(NavbarLink): add new fields

--- src/components/NavbarLink.tsx
+++ src/components/NavbarLink.tsx
@@ -4,28 +4,34 @@ import Image from "next/image"
 
 export default function NavbarLink({
   link,
-  pathname
+  pathname,
+  imageClassName,
+  imageDivClassName,
+  linkNameClassName
 }: {
   link: NavbarLinkProps
-  pathname: string
+  pathname?: string
+  imageClassName?: string
+  imageDivClassName?: string
+  linkNameClassName?: string
 }) {
   return (
     <li
-      className={`h-max hover:bg-gray-2 border-b-[3px] duration-150 p-2 ${
+      className={`h-max rounded-md hover:bg-gray-2 border-b-[3px] duration-150 p-2 ${
         pathname === link?.name ? " border-b-pink-2" : "border-b-transparent"
       }`}
     >
       <Link href={link?.href} className="flex flex-col items-center gap-2">
-        <div className={`rounded-full p-2 ${link?.backgroundColor}`}>
+        <div className={`rounded-full p-2 ${link?.backgroundColor} ${imageDivClassName}`}>
           <Image
-            className="w-3 h-3 md:w-5 md:h-5"
+            className={`w-3 h-3 md:w-5 md:h-5 ${imageClassName}`}
             src={link?.icon}
             width={20}
             height={20}
             alt={`${link?.name} icon`}
           />
         </div>
-        <p className="text-center text-sm md:text-base">{link?.name}</p>
+        <p className={`text-center text-sm md:text-base ${linkNameClassName}`}>{link?.name}</p>
       </Link>
     </li>
   )
