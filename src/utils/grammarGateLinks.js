f6450090dabe91641b646e83398b369d2d77079c
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-27 08:48:40 -0300

    feat: add grammarGateLinks

--- src/utils/grammarGateLinks.js
+++ src/utils/grammarGateLinks.js
@@ -6,13 +6,15 @@ const links = [
     name: "Grammar Checker",
     href: "/grammar-checker",
     icon: grammarCheckerIcon,
-    backgroundColor: "bg-pink-1"
+    backgroundColor: "bg-pink-1",
+    borderColor: "border-b-pink-1"
   },
   {
     name: "Text Summarizer",
     href: "/text-summarizer",
     icon: textSummarizer,
-    backgroundColor: "bg-green-500"
+    backgroundColor: "bg-green-500",
+    borderColor: "border-b-green-500"
   }
 ]
 
