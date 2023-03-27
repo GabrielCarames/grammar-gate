8fa94efcc3441431e0340d4ca5f2e3d4b4d61e9c
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-27 17:26:18 -0300

    feat(Navbar): add home link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router"
 import links from "../utils/grammarGateLinks"
 import { Item } from "./GrammarGateItem"
+import Link from "next/link"
 
 export default function Navbar() {
   const router = useRouter()
@@ -9,7 +10,9 @@ export default function Navbar() {
     <section className="w-full h-max max-h-32 bg-gray-1 border-r border-r-gray-2 mb-5 2xl:mb-0">
       <div className="w-full h-full flex flex-wrap lg:flex-row md:flex-nowrap items-center justify-center lg:justify-start">
         <div className="w-full md:max-w-max px-5 py-2 border-b-[0.5px] md:border-none border-gray-3">
-          <h2 className="text-base md:text-lg font-semibold">Grammar Gate</h2>
+          <Link href="/">
+            <h2 className="text-base md:text-lg font-semibold">Grammar Gate</h2>
+          </Link>
         </div>
         <nav className="w-full flex justify-center border-b-[0.5px] md:border-none border-gray-3">
           <ul className="flex gap-1 px-5 h-full">
