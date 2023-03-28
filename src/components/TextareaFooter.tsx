a6e23db2fdac5c10f8ee254e659301313cce9eff
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-28 15:10:07 -0300

    refactor(TextareaFooter): replace for components

--- src/components/TextareaFooter.tsx
+++ src/components/TextareaFooter.tsx
@@ -1,47 +1,26 @@
 import { useTextareaFooter } from "@/hooks/useTextareaFooter"
-import { ThreeDots } from "react-loader-spinner"
+import Button from "./Button"
+import TextareaSpan from "./TextareaSpan"
 
 export default function TextareaFooter({ loading }: { loading: boolean }) {
   const { corrections, wordsCount, correctionsCount, fixEverything } = useTextareaFooter()
 
+  const availableData = corrections.corrections.length >= 1
+
   return (
     <div className="w-full h-12 flex justify-between items-center px-5 text-sm md:text-base">
-      <p className="w-min md:w-auto flex flex-wrap gap-x-1 justify-center items-center text-xs lg:text-base">
-        <span className="text-yellow-1">{wordsCount()}</span> Words
-      </p>
-      <button
-        className={`p-4 py-1 rounded-full text-sm md:text-md font-bold duration-200
-        ${
-          corrections.corrections.length >= 1
-            ? "!bg-pink-1 hover:!bg-pink-2 !text-white"
-            : "bg-gray-500 text-gray-600"
-        }
-        ${
-          corrections.corrections.length <= 0 && loading
-            ? "!bg-pink-1"
-            : "bg-gray-500 text-gray-600"
-        }
-        `}
+      <TextareaSpan text="Words" data={wordsCount()} />
+      <Button
+        availableData={availableData}
         onClick={fixEverything}
-        disabled={corrections.corrections.length <= 0}
-      >
-        {!loading ? (
-          <span>Fix everything</span>
-        ) : (
-          <ThreeDots
-            height="24"
-            width="50"
-            radius="9"
-            color="#F9F871"
-            ariaLabel="three-dots-loading"
-            wrapperStyle={{}}
-            visible={true}
-          />
-        )}
-      </button>
-      <p className="w-min md:w-auto flex flex-wrap gap-x-1 justify-center items-center text-xs lg:text-base">
-        <span className="text-red-500 text-center">{correctionsCount()}</span> Mistakes
-      </p>
+        loading={loading}
+        text="Fix everything"
+      />
+      <TextareaSpan
+        text="Mistakes"
+        data={correctionsCount()}
+        className="!text-red-500 text-center"
+      />
     </div>
   )
 }
