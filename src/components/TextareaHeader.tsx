f7ebf63649d06030488b483331b517026e76fcff
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-28 08:47:26 -0300

    feat(TextareaHeader): replace data with props

--- src/components/TextareaHeader.tsx
+++ src/components/TextareaHeader.tsx
@@ -1,13 +1,16 @@
 import InfoButtonSection from "./InfoButtonSection"
 
-export default function TextareaHeader() {
+export default function TextareaHeader({
+  title,
+  description
+}: {
+  title: string
+  description: string
+}) {
   return (
     <header className="bg-pink-1 py-8 px-5 h-max mb-10 rounded-md flex justify-between items-center">
       <h1 className="text-base md:text-lg lg:text-xl font-bold leading-none text-center">
-        Grammar Checker |{" "}
-        <span className="font-normal text-base md:text-base">
-          Write your text and let us check your grammar!
-        </span>
+        {title} | <span className="font-normal text-base md:text-base">{description}</span>
       </h1>
       <InfoButtonSection />
     </header>
