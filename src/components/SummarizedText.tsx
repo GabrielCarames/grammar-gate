013aa614720bc68ff5f6dcc9b98f8cb45d2739e5
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-30 11:29:03 -0300

    feat(SummarizedText): add loading skeleton

--- src/components/SummarizedText.tsx
+++ src/components/SummarizedText.tsx
@@ -1,5 +1,6 @@
 import TextareaSpan from "./TextareaSpan"
 import { useSummaryContext } from "@/contexts/SummaryContext"
+import useAxios from "@/hooks/useAxios"
 
 const SummarizedText = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,17 +12,29 @@ const SummarizedText = ({ children }: { children: React.ReactNode }) => {
 
 SummarizedText.SummarizedTextContent = function SummarizedTextContent() {
   const { summary } = useSummaryContext()
+  const { loading } = useAxios()
 
   return (
     <div className="max-h-[250px] md:max-h-[500px] h-max md:h-screen p-5 text-base lg:text-lg text-white">
-      <textarea
-        className="w-full max-h-[250px] md:max-h-[500px] bg-gray-1 h-max md:h-screen outline-none resize-none text-base lg:text-lg"
-        name="text"
-        cols={30}
-        rows={10}
-        defaultValue={summary}
-        spellCheck={false}
-      />
+      {loading ? (
+        <div className="flex flex-col gap-2 w-full max-h-[240px] md:max-h-[490px] bg-gray-1 h-max md:h-screen text-base lg:text-lg animate-pulse">
+          <div className="w-full h-5 bg-gray-2 rounded-md"></div>
+          <div className="w-[75%] h-5 bg-gray-2 rounded-md"></div>
+          <div className="w-[25%] h-5 bg-gray-2 rounded-md"></div>
+          <div className="w-[50%] h-5 bg-gray-2 rounded-md"></div>
+        </div>
+      ) : (
+        <textarea
+          className="w-full max-h-[250px] md:max-h-[500px] bg-gray-1 h-max md:h-screen outline-none resize-none text-base lg:text-lg"
+          name="text"
+          cols={30}
+          rows={10}
+          defaultValue={summary}
+          spellCheck={false}
+          readOnly={summary.length <= 0}
+          disabled={summary.length <= 0}
+        />
+      )}
     </div>
   )
 }
