201dc7ac4595b471b6073452a6fd85121f89625d
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-30 10:57:11 -0300

    feat: add Summary context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,17 @@ import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import "@/styles/globals.css"
+import { SummaryProvider } from "@/contexts/SummaryContext"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ValueProvider>
-      <CorrectionsProvider>
-        <ToastContainer />
-        <Component {...pageProps} />
-      </CorrectionsProvider>
+      <SummaryProvider>
+        <CorrectionsProvider>
+          <ToastContainer />
+          <Component {...pageProps} />
+        </CorrectionsProvider>
+      </SummaryProvider>
     </ValueProvider>
   )
 }
