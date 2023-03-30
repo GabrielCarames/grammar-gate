f994b5c4e69ae569ff4768d8f214ed57a0e4db22
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-30 11:26:53 -0300

    feat: add LoadingContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
-import { CorrectionsProvider } from "@/contexts/CorrectionsContext"
-import { ValueProvider } from "@/contexts/ValueContext"
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
+import { CorrectionsProvider } from "@/contexts/CorrectionsContext"
+import { ValueProvider } from "@/contexts/ValueContext"
+import { SummaryProvider } from "@/contexts/SummaryContext"
+import { LoadingProvider } from "@/contexts/LoadingContext"
 import "react-toastify/dist/ReactToastify.css"
 import "@/styles/globals.css"
-import { SummaryProvider } from "@/contexts/SummaryContext"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ValueProvider>
-      <SummaryProvider>
-        <CorrectionsProvider>
-          <ToastContainer />
-          <Component {...pageProps} />
-        </CorrectionsProvider>
-      </SummaryProvider>
-    </ValueProvider>
+    <LoadingProvider>
+      <ValueProvider>
+        <SummaryProvider>
+          <CorrectionsProvider>
+            <ToastContainer />
+            <Component {...pageProps} />
+          </CorrectionsProvider>
+        </SummaryProvider>
+      </ValueProvider>
+    </LoadingProvider>
   )
 }
