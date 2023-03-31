a4c829e0173e68ed8e4554db3c9daf962a1ea50a
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-03-31 11:12:12 -0300

    feat(tailwind): add custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+  screens: {
+      'xs': '400px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         "black": "#03041A",
