307ea76128ac1fdd6cf9f1de44842a96bc56ea0c
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-03 17:14:11 -0300

    remove: remove "SummaryContext"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
-import { SummaryProvider } from "@/contexts/SummaryContext"
 import { LoadingProvider } from "@/contexts/LoadingContext"
 import ThemeWrapper from "@/components/ThemeWrapper"
 import { ThemeProvider } from "next-themes"
@@ -13,12 +12,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <HeadWrapper>
       <ThemeProvider>
         <LoadingProvider>
-          <SummaryProvider>
-            <ThemeWrapper>
-              <ToastContainer limit={2} />
-              <Component {...pageProps} />
-            </ThemeWrapper>
-          </SummaryProvider>
+          <ThemeWrapper>
+            <ToastContainer limit={2} />
+            <Component {...pageProps} />
+          </ThemeWrapper>
         </LoadingProvider>
       </ThemeProvider>
     </HeadWrapper>
