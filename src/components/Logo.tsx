94d6850c84cd14235562399466c1cc99269109be
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-03 20:50:11 -0300

    feat: add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import ThemeButton from "./ThemeButton"
+
+export default function Logo() {
+  return (
+    <div className="w-full md:max-w-max flex justify-between items-center py-2 border-b-[0.5px] md:border-none border-white-2 dark:border-gray-3 px-5 md:px-0">
+      <Link href="/">
+        <h2 className="text-base md:text-lg text-gray-1 dark:text-white">
+          <span className="text-pink-1 font-bold text-xl">G</span>
+          rammar Gate
+        </h2>
+      </Link>
+      <ThemeButton />
+    </div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { useRouter } from "next/router"
-import Link from "next/link"
 import { Item } from "./GrammarGateItem"
 import links from "../utils/grammarGateLinks"
 import IconWrapper from "./IconWrapper"
 import ThemeButton from "./ThemeButton"
+import Logo from "./Logo"
 
 export default function Navbar() {
   const router = useRouter()
@@ -11,15 +11,7 @@ export default function Navbar() {
   return (
     <section className="w-full h-max bg-white dark:bg-gray-1 border-r dark:border-r-gray-2 mb-5 2xl:mb-0 shadow-md">
       <div className="w-full h-full flex flex-wrap lg:flex-row md:flex-nowrap items-center justify-center lg:justify-start md:px-5">
-        <div className="w-full md:max-w-max flex justify-between items-center py-2 border-b-[0.5px] md:border-none border-white-2 dark:border-gray-3 px-5 md:px-0">
-          <Link href="/">
-            <h2 className="text-base md:text-lg text-gray-1 dark:text-white">
-              <span className="text-pink-1 font-bold text-xl">G</span>
-              rammar Gate
-            </h2>
-          </Link>
-          <ThemeButton />
-        </div>
+        <Logo />
         <nav className="w-full flex justify-center border-b-[0.5px] md:border-none border-white-2 dark:border-gray-3">
           <ul className="flex gap-1 px-5 h-full">
             {links?.map(link => (
