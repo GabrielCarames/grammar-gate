a88e9491e2a5683fa3141ef99e6e2e687fdfb6d7
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-03 17:35:17 -0300

    fix(grammarCheckerPrompt): fix prompt

--- src/utils/grammarCheckerPrompt.ts
+++ src/utils/grammarCheckerPrompt.ts
@@ -1,15 +1,15 @@
 export const createFirstPrompt = (textToCheck: string) => {
   const firstPrompt = `
-    Necesito que hagas de corrector gramatical de ingles. Te daré un texto en ingles y tendras que corregirlo indicando que palabra/s estan mal, como deberian ser corregidas, y él por que.
-    Mas allá del significado correcto del texto, tambien necesitaría que indiques donde podrían ir comas, puntos, etc.
-    Tu respuesta siempre tiene que ser en un formato JSON válido. Agrega las comillas dobles a cada clave de "corrections" y "textCorrected". Nunca me respondas con otra frase extra, ni me expliques nada sin que yo te lo pida.
+    Necesito que hagas de corrector gramatical de inglés. Te daré un texto en inglés y tendras que corregirlo indicando que palabra/s están mal, como deberían ser corregidas, y el por qué.
+    Mas allá del significado correcto del texto, también necesitaría que indiques donde podrían ir comas, puntos, etc.
+    Tu respuesta siempre tiene que ser en un formato JSON válido. Agrega las comillas dobles a cada clave de "corrections" y "correctedText". Nunca me respondas con otra frase extra, ni me expliques nada sin que yo te lo pida.
 
     Fijate en este ejemplo de como debes responderme en formato JSON: 
     {
       "corrections": 
         [
           {
-            "id": "id unica generada por ti",
+            "id": "id única generada por ti",
             "type": "tipo de correción mistake",
             "explanation": "explanation",
             "result": ["palabra u oración que tengas que corregir", "palabra u oración ya corregida"]
@@ -29,12 +29,12 @@ export const createFirstPrompt = (textToCheck: string) => {
   return firstPrompt
 }
 
-export const createConsecutivePrompt = (textToCheck: string, previousTextCorrected: string) => {
+export const createConsecutivePrompt = (textToCheck: string, previousCorrectedText: string) => {
   const consecutivePrompt = `
     Ahora corrige este nuevo texto pero ten en cuenta que te voy a dar el texto anterior que corregiste, por lo que si este nuevo texto ya contiene el texto corregido, no lo corrigas de nuevo.
-    Tu respuesta siempre tiene que ser en un formato JSON válido. Agrega las comillas dobles a cada clave de "corrections" y "textCorrected". Nunca me respondas con otra frase extra, ni me expliques nada sin que yo te lo pida.
+    Tu respuesta siempre tiene que ser en un formato JSON válido. Agrega las comillas dobles a cada clave de "corrections" y "correctedText". Nunca me respondas con otra frase extra, ni me expliques nada sin que yo te lo pida.
 
-    Texto que corregiste en la anterior petición: ${previousTextCorrected}
+    Texto que corregiste en la anterior petición: ${previousCorrectedText}
     Nuevo texto para corregir: ${textToCheck}
   `
   return consecutivePrompt
