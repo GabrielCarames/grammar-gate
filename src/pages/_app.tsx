5e555a692001e0382580b5fc83dd2825a5ac528f
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-03 16:52:04 -0300

    change: remove "CorrectionsContext"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
-import { CorrectionsProvider } from "@/contexts/CorrectionsContext"
 import { SummaryProvider } from "@/contexts/SummaryContext"
 import { LoadingProvider } from "@/contexts/LoadingContext"
 import { ValueProvider } from "@/contexts/ValueContext"
@@ -17,12 +16,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <LoadingProvider>
           <ValueProvider>
             <SummaryProvider>
-              <CorrectionsProvider>
-                <ThemeWrapper>
-                  <ToastContainer limit={2} />
-                  <Component {...pageProps} />
-                </ThemeWrapper>
-              </CorrectionsProvider>
+              <ThemeWrapper>
+                <ToastContainer limit={2} />
+                <Component {...pageProps} />
+              </ThemeWrapper>
             </SummaryProvider>
           </ValueProvider>
         </LoadingProvider>
