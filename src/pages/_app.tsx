b2b14efa889bb725bbe66a5c067c66b976ec1d45
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-03 17:18:27 -0300

    remove: remove "LoadingContext"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,19 @@
 import type { AppProps } from "next/app"
 import { ToastContainer } from "react-toastify"
-import { LoadingProvider } from "@/contexts/LoadingContext"
-import ThemeWrapper from "@/components/ThemeWrapper"
 import { ThemeProvider } from "next-themes"
+import ThemeWrapper from "@/components/ThemeWrapper"
+import HeadWrapper from "@/components/HeadWrapper"
 import "react-toastify/dist/ReactToastify.css"
 import "@/styles/globals.css"
-import HeadWrapper from "@/components/HeadWrapper"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <HeadWrapper>
       <ThemeProvider>
-        <LoadingProvider>
-          <ThemeWrapper>
-            <ToastContainer limit={2} />
-            <Component {...pageProps} />
-          </ThemeWrapper>
-        </LoadingProvider>
+        <ThemeWrapper>
+          <ToastContainer limit={2} />
+          <Component {...pageProps} />
+        </ThemeWrapper>
       </ThemeProvider>
     </HeadWrapper>
   )
