af016293c46be6ce1eedf6ba8a959135ff6314ec
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-11 18:19:51 -0300

    fix(Correction): fix white mode text

--- src/components/Correction.tsx
+++ src/components/Correction.tsx
@@ -9,7 +9,9 @@ export default function Correction({ correction }: { correction: CorrectionProps
     <li className="flex flex-col gap-2 border-[2px] border-white-2 dark:border-gray-2 p-2 rounded-md">
       <p className="capitalize font-bold">{correction.type}</p>
       <CorrectionComparation correction={correction} />
-      <p className="text-sm lg:text-base">{correction.explanation}</p>
+      <p className="text-sm lg:text-base text-text-gray dark:text-white first-letter:capitalize">
+        {correction.explanation}
+      </p>
       <button
         className="bg-pink-1 text-center py-1 rounded-md hover:bg-pink-2 duration-150 text-white black:text-black"
         onClick={() => handleCorrection(correction)}
