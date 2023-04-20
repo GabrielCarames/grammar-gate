1d891adc3d13e8c04aa949fdd8346921a1148832
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-20 13:59:59 -0300

    feat(enums): add translator enums

--- src/enums.d.ts
+++ src/enums.d.ts
@@ -1,11 +1,13 @@
 export enum HeaderTitle {
   GRAMMAR_CHECKER = "Grammar Checker",
-  TEXT_SUMMARIZER = "Text Summarizer"
+  TEXT_SUMMARIZER = "Text Summarizer",
+  TRANSLATOR = "Translator"
 }
 
 export enum HeaderDescription {
   GRAMMAR_CHECKER = "Write your text and let us check your grammar!",
-  TEXT_SUMMARIZER = "Summarize any text in a few seconds!"
+  TEXT_SUMMARIZER = "Summarize any text in a few seconds!",
+  TRANSLATOR = "Choose a language and translate your text!"
 }
 
 export const AUTO_LANGUAGE = "auto"
