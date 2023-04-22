8667dbfcc43dea0dcb11d7884c8183dd2fcf394d
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-22 11:39:59 -0300

    feat(LanguageSelector): add custom arrow button

--- src/components/Translator/LanguageSelector.tsx
+++ src/components/Translator/LanguageSelector.tsx
@@ -1,6 +1,8 @@
 import Select from "../Select"
 import { type FC } from "react"
 import { AUTO_LANGUAGE, Languages } from "@/enums.d"
+import BottomArrow from "../../../public/bottom-arrow-icon.svg"
+import IconWrapper from "../IconWrapper"
 
 interface LanguageSelectorProps {
   onSelectChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
@@ -22,11 +24,24 @@ const LanguageSelector: FC<LanguageSelectorProps> = ({
   children
 }) => {
   return (
-    <div className="flex flex-1 flex-col gap-2">
-      <Select onChange={onSelectChange} value={value} name={selectName} defaultValue={defaultValue}>
+    <div className="flex flex-1 flex-col gap-2 relative">
+      <Select
+        onChange={onSelectChange}
+        value={value}
+        name={selectName}
+        defaultValue={defaultValue}
+        className={`h-10 rounded-md appearance-none bg-no-repeat bg-right pl-5 py-2 pr-10 cursor-pointer border dark:border-gray-2`}
+      >
         {defaultOption && <Select.Option options={defaultOption} />}
         <Select.Option options={options} />
       </Select>
+      <button className="pointer-events-none absolute top-0 right-0 pr-5 h-[40px] flex items-center">
+        <IconWrapper
+          icon={<BottomArrow className="w-7 h-7" />}
+          className="!w-7"
+          alt="Toggle dark mode"
+        />
+      </button>
       {children}
     </div>
   )
