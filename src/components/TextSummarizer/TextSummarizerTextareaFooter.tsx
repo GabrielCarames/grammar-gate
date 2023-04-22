e942a4d1dd1ad81c2ea19d7a30fc45874204fbc7
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-22 13:25:13 -0300

    feat: add TextareaFooter component

--- src/components/TextSummarizer/TextSummarizerTextareaFooter.tsx
+++ src/components/TextSummarizer/TextSummarizerTextareaFooter.tsx
@@ -3,6 +3,7 @@ import SummaryLength from "../SummaryLength"
 import Button from "../Button"
 import Span from "../Span"
 import { wordsCount } from "@/utils/wordsCount"
+import TextareaFooter from "../TextareaFooter"
 
 interface TextSummarizerTextareaFooterProps {
   value: string
@@ -14,7 +15,7 @@ const TextSummarizerTextareaFooter: FC<TextSummarizerTextareaFooterProps> = ({
   loading
 }) => {
   return (
-    <footer className="w-full h-max flex flex-wrap flex-col sm:flex-row justify-center gap-y-2 sm:justify-between items-center px-0 sm:px-5 py-2 border-t-[.5px] border-white-2 dark:border-gray-2">
+    <TextareaFooter>
       <div className="h-max flex xs:justify-center gap-1 xs:gap-5 md:justify-start">
         <div className="border-[.5px] dark:border-gray-2 sm:border-none sm:p-0 p-1 px-1 rounded-md flex items-center text-text-gray dark:text-white">
           <Span text="Words" data={wordsCount(value)} />
@@ -32,7 +33,7 @@ const TextSummarizerTextareaFooter: FC<TextSummarizerTextareaFooterProps> = ({
           ${value.length <= 0 ? "!bg-gray-500 text-white-2 dark:text-gray-600" : ""}`}
         />
       </div>
-    </footer>
+    </TextareaFooter>
   )
 }
 
---
+++ src/components/TextareaFooter.tsx
@@ -0,0 +1,15 @@
+import { type FC } from "react"
+
+interface TextareaFooterProps {
+  children: React.ReactNode
+}
+
+const TextareaFooter: FC<TextareaFooterProps> = ({ children }) => {
+  return (
+    <footer className="w-full h-max flex flex-wrap flex-col sm:flex-row justify-center gap-y-2 sm:justify-between items-center px-0 sm:px-5 py-2 border-t-[.5px] border-white-2 dark:border-gray-2">
+      {children}
+    </footer>
+  )
+}
+
+export default TextareaFooter
