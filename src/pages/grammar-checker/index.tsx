048026d6c80be5858e2c23bef0b3875cbc643f65
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2023-04-23 18:40:36 -0300

    fix: fix json imports

--- src/pages/grammar-checker/index.tsx
+++ src/pages/grammar-checker/index.tsx
@@ -1,4 +1,4 @@
-import GrammarCheckerInfo from "../../utils/GrammarCheckerInfo.json"
+import GrammarCheckerInfo from "../../utils/grammarCheckerInfo.json"
 import GrammarCheckerTextareaWrapper from "@/components/GrammarChecker/GrammarCheckerTextareaWrapper"
 import TextareaSection from "@/components/TextareaSection"
 import Aside from "@/components/GrammarChecker/Aside"
